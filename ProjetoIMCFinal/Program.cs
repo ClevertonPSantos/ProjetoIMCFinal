c066584de5fccffc0e3cb9c00dd71491d788a3c1
Cleverton Santos <clevertonpsantos@hotmail.com>
2022-06-14 23:29:34 -0300

    Projeto IMC iniciado

---
+++ ProjetoIMCFinal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjetoIMCFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primeiro vamos criar as variáveis
+            string nome;
+            string sexo;
+            string categoria;
+            int idade;
+            double altura, peso, resultAltura;
+            bool verificar = false;
+            
+            Console.WriteLine("\nSeja bem vindo(a), irei conhecer um pouco melhor você. Vamos começar!!!");
+
+            //Pedir os dados iniciais
+            Console.WriteLine("\nQual o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine("\nTudo bem " + nome +", vamos continuar...");
+
+            Console.WriteLine("\nQual a sua idade?");
+            idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nOk " + nome + ", já anotamos a sua idade de " + idade + " anos");
+        }
+    }
+}
